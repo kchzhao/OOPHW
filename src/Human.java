c224b55d21d5170f171fb487659364873d7e2826
kchzhao <kamila.chzhao@gmail.com>
2022-11-20 00:07:38 -0500

    Second commit

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,19 @@
 public class Human {
+    private int yearOfBirth;
+    private String name;
+    private String town;
+
+    public Human(int birthYear, String name, String town){
+        yearOfBirth = birthYear;
+        this.name = name;
+        this.town = town;
+    }
+
+    @Override
+    public String toString(){
+        return "Привет! Меня зовут " + this.name +
+                ". Я из города " + this.town + ". Я родился в " + this.yearOfBirth
+                + " году. Будем знакомы!";
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Human Maksim = new Human(1988, "Максим", "Минск");
+        Human Anya = new Human(1993, "Аня", "Москва");
+        Human Katya = new Human(1992, "Катя", "Калининград");
+        Human Artem = new Human(1995, "Артем", "Москва");
 
-        System.out.println("Hello world!");
+        System.out.println(Maksim.toString());
+        System.out.println(Anya.toString());
+        System.out.println(Katya.toString());
+        System.out.println(Artem.toString());
     }
 }
